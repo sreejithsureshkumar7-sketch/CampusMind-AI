1f82c3de64dba8e9ceac1bb933bfe088d7808b9d
Sreejith S <sreejithsureshkumar7@gmail.com>
2026-06-11 22:34:36 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Paste your Firebase web config here.
 // Keep this format. Do not use import/export in this file.
 window.firebaseConfig = {
-  apiKey: "AIzaSyB31l66TZR-0WJszsyp0LznOd62bj5reoQ",
-    authDomain: "campusmind-ai-analytics-cc29a.firebaseapp.com",
-    projectId: "campusmind-ai-analytics-cc29a",
-    storageBucket: "campusmind-ai-analytics-cc29a.firebasestorage.app",
-    messagingSenderId: "843261164399",
-    appId: "1:843261164399:web:dcb8916d2b33a78aeb069a"
+apiKey: "AIzaSyB31l66TZR-0WJszsyp0LznOd62bj5reoQ",
+  authDomain: "campusmind-ai-analytics-cc29a.firebaseapp.com",
+  projectId: "campusmind-ai-analytics-cc29a",
+  storageBucket: "campusmind-ai-analytics-cc29a.firebasestorage.app",
+  messagingSenderId: "843261164399",
+  appId: "1:843261164399:web:dcb8916d2b33a78aeb069a"
 };
