1a83d2a80a107d56d02c4eb899027d8b92380910
Sreejith S <sreejithsureshkumar7@gmail.com>
2026-06-11 16:25:51 +0530

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB31l66TZR-0WJszsyp0LznOd62bj5reoQ",
+  authDomain: "campusmind-ai-analytics-cc29a.firebaseapp.com",
+  projectId: "campusmind-ai-analytics-cc29a",
+  storageBucket: "campusmind-ai-analytics-cc29a.firebasestorage.app",
+  messagingSenderId: "843261164399",
+  appId: "1:843261164399:web:dcb8916d2b33a78aeb069a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
